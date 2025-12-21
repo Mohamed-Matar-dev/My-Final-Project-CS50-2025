824a18c03bc7434058049219dc828cc2a76ce101
Mohamed Matar <mohamed.matar.p@gmail.com>
2025-12-21 19:11:14 +0200

    Adding hamberger and delete logic

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
+# default route/page showing the tasks
 @app.route("/")
 def index():
     db = get_db()
@@ -25,6 +25,7 @@ def index():
 
     return render_template("index.html", tasks=tasks)
 
+# add task route 
 @app.route("/add-task", methods=["GET", "POST"])
 def add_task():
     if request.method =="POST":
@@ -36,6 +37,8 @@ def add_task():
         return redirect("/")
     return render_template("add-task.html")
 
+# function to complete the task (check box), dosn't need correspodning HTML.
+# It shows in index.html, and it worked with home.js
 @app.route('/complete-task', methods=['POST'])
 def complete_task():
     data = request.json or {}
@@ -46,6 +49,20 @@ def complete_task():
     
     db = get_db()
     db.execute("UPDATE tasks SET complete = ? WHERE id = ?", (completed, task_id))
+    db.commit()
+    db.close()
+
+    return "", 204
+
+@app.route('/delete-task', methods=["POST"])
+def delete_task():
+    data = request.json or {}
+    task_id = data.get("id")
+    if not task_id:
+        return "Error", 400
+    
+    db = get_db()
+    db.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
     db.commit()
     db.close()
 
--- static/home.js
+++ static/home.js
@@ -1,16 +1,20 @@
+// Ongoin tasks ordered list 
 const ongoingContainer = document.querySelector("#ongoing-tasks");
+// completed tasks ordered list
 const completedContainer = document.querySelector("#completed-tasks");
-
+// check box for all the list items
 const checkBox = document.querySelectorAll('input[type="checkbox"]');
 
+// making the check box works as intented
 checkBox.forEach(box => {
   box.addEventListener("change", (e) => {
     const li = e.target.closest("li");
     if (!li) return;
 
+    // checking if task completet
     const completed = e.target.checked;
 
-    // Update database
+    // Send Update database request to app.py
     fetch("/complete-task", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -27,3 +31,40 @@ checkBox.forEach(box => {
     }
   });
 });
+
+// menu to show options on the page 
+const taskMenuBtn = document.querySelectorAll(".task-menu-btn");
+
+const editBtn = document.querySelectorAll(".edit-btn");
+const deleteBtn = document.querySelectorAll(".delete-btn");
+// the logic so each menu button work as way to show options
+taskMenuBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const li = event.target.closest("li");
+    const options = li.querySelector(".task-options");
+    options.classList.toggle("d-none");
+  });
+});
+// delete task logic 
+deleteBtn.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const li = e.target.closest("li");
+    const taskId = li.id;
+    // sent post request to flask
+    fetch("/delete-task", {
+      method: "POST",
+      headers: {"Content-type": "application/json"},
+      body: JSON.stringify({id: taskId})
+    }).then(response => {         // after the request sent wait for the return
+      if (response.ok){
+        li.remove(); // remove from the DOM
+      }else {
+        alert("Faild to delete task");
+      }
+    });
+
+  });
+});
+
